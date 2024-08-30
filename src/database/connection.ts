ca711f21558cfdb05f74edbcf211b1885bd97b3c
Tien Anh <anhnt2@yopaz.vn>
2024-08-30 14:33:06 +0700

    feat: update db dialect options

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,7 +4,13 @@ import dbConfig from './configs'
 const sequelizeConnection: Sequelize = new Sequelize(dbConfig.DB_DATABASE, dbConfig.DB_USERNAME, dbConfig.DB_PASSWORD, {
   host: dbConfig.DB_HOST,
   dialect: 'mysql',
-  port: 3306
+  port: 3306,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // This is important for Cloud SQL
+    }
+  }
 })
 
 console.log({
