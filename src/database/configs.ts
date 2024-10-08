ca0cb73a7df44bef9ff47039d28d09027df497f0
Tien Anh <anhnt2@yopaz.vn>
2024-10-08 16:14:25 +0700

    fix: deploy to cloud run

--- src/database/configs.ts
+++ src/database/configs.ts
@@ -7,7 +7,8 @@ const dbConfig = {
   DB_DATABASE: process.env.DB_NAME || '',
   DB_HOST: process.env.DB_HOST || '',
   DB_USERNAME: process.env.DB_USER || '',
-  DB_PASSWORD: process.env.DB_PASSWORD || ''
+  DB_PASSWORD: process.env.DB_PASSWORD || '',
+  DB_CLOUD_SQL_CONNECTION_NAME: process.env.DB_CLOUD_SQL_CONNECTION_NAME || ''
 }
 
 export default dbConfig
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,24 +2,26 @@ import { Sequelize } from 'sequelize'
 import dbConfig from './configs'
 
 const sequelizeConnection: Sequelize = new Sequelize(dbConfig.DB_DATABASE, dbConfig.DB_USERNAME, dbConfig.DB_PASSWORD, {
-  host: dbConfig.DB_HOST,
+  // host: dbConfig.DB_HOST,
   dialect: 'mysql',
   port: 3306,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // This is important for Cloud SQL
+      rejectUnauthorized: false
     }
-  }
+  },
+  host: '/cloudsql/' + dbConfig.DB_CLOUD_SQL_CONNECTION_NAME
 })
 
 console.log({
   username: dbConfig.DB_USERNAME,
   password: dbConfig.DB_PASSWORD,
   database: dbConfig.DB_DATABASE,
-  host: dbConfig.DB_HOST,
+  // host: dbConfig.DB_HOST,
   dialect: 'mysql',
-  port: 3306
+  port: 3306,
+  host: '/cloudsql/' + dbConfig.DB_CLOUD_SQL_CONNECTION_NAME
 })
 
 export default sequelizeConnection
